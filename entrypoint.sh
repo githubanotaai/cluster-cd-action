82a8e22827510bfc25b4821d1907944d60135b08
Rafael Souza <razuos@protonmail.com>
2021-09-28 08:12:17 -0300

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -94,7 +94,7 @@ set_tag_on_yamls() {
   for YAML_PATH in "${DEPLOYMENT_REPO_YAML_PATHS[@]}"; do
     YAML_PATH="$( echo $DEPLOYMENT_REPO_PATH/$YAML_PATH | sed 's/ENV/'$ENVIRONMENT'/g' )"
     echo "Editing YAML: $YAML_PATH"
-    yq w -i ${YAML_PATH} ${IMGTAG_KEY} ${IMAGE_TAG} || exit 1
+    yq w -i ${YAML_PATH} ${IMGTAG_KEY} ${IMAGE_TAG}
     cd "$DEPLOYMENT_REPO_PATH"
     git add "$YAML_PATH"
     cd "$OLDPWD"
